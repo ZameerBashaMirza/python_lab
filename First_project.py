5caaafdd17574abf71b7ddd811b95cd1cf50bc4b
Zameer Basha Mirza <zameerbashamirza@gmail.com>
2022-12-04 13:52:30 +0530

    strings

--- First_project.py
+++ First_project.py
@@ -1,21 +1,3 @@
-print("hello 'world'")
-print("Learning with 'TOTAL Python' is super fun!")
-print(550+5)
-
-print('100'+'50')
-print('Fede')
-
-print("Hello"+'Fede')
-print("Hello"+"  "+'Fede')
-print("Hello  "+'Fede')
-print("Hello"+'  Fede')
-
-print("My name is \"Federico ")
-
-print("This is one line\nand this is another")
-print("\tthis is the third line")
-
-print('This isn\'t a number')
-print("This isn't a new line")
-
-print('This sign \\ is a backslash')
+print("Line 1\nLine 2\nLine 3")
+print("\tA\tB\tC\n\tD\tE\tF\n\tG\tH\tI\n")
+print("Backslash (\)\nForward Slash (/)")
