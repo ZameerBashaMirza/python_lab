fb52f2896375c0184926c085a4f52efa365f2d4e
Zameer Basha Mirza <zameerbashamirza@gmail.com>
2022-12-04 13:24:37 +0530

    namespaces

---
+++ First_project.py
@@ -0,0 +1,21 @@
+print("hello 'world'")
+print("Learning with 'TOTAL Python' is super fun!")
+print(550+5)
+
+print('100'+'50')
+print('Fede')
+
+print("Hello"+'Fede')
+print("Hello"+"  "+'Fede')
+print("Hello  "+'Fede')
+print("Hello"+'  Fede')
+
+print("My name is \"Federico ")
+
+print("This is one line\nand this is another")
+print("\tthis is the third line")
+
+print('This isn\'t a number')
+print("This isn't a new line")
+
+print('This sign \\ is a backslash')
